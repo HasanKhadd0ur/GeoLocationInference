b16326916f362f3392fa13caa880120b58e988bb
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-07-05 09:57:09 +0300

    Add Extractin Evvent Location

--- src/app/core/services/location/base/recognition_service_base.py
+++ src/app/core/services/location/base/recognition_service_base.py
@@ -6,6 +6,12 @@ from app.core.configs.base_config import BaseConfig
 class IRecognitionService(ABC):
     def __init__(self,config :BaseConfig):
         self.config =config
+    
     @abstractmethod
     def extract_message_location(self, text: str) -> str:
         pass
+    
+    @abstractmethod
+    def extract_event_location(self, messages: List[str]) -> str:
+        pass
+
